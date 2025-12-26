fd841cf3ab25b8d4f0ee301a7d0a522739d15e6f
Roy Moore <roy@moore.co.il>
2025-12-26 22:31:34 +0200

    Using alive-progress

--- gradient.py
+++ gradient.py
@@ -1,16 +1,19 @@
 # Generate a gradient image
 
 from PIL import Image
+from alive_progress import alive_bar
 
 def generate_gradient_image(width=256, height=256, filename="gradient.png"):
     image = Image.new("RGB", (width, height))
-    for x in range(width):
-        for y in range(height):
-            r = float(x / (width - 1))
-            g = float(y / (height - 1))
-            b = 0.0
-            ir = int(r * 255.999)
-            ig = int(g * 255.999)
-            ib = int(b * 255.999)
-            image.putpixel((x, y), (ir, ig, ib))
+    with alive_bar(width * height, title="Generating Gradient Image") as bar:
+        for x in range(width):
+            for y in range(height):
+                r = float(x / (width - 1))
+                g = float(y / (height - 1))
+                b = 0.0
+                ir = int(r * 255.999)
+                ig = int(g * 255.999)
+                ib = int(b * 255.999)
+                image.putpixel((x, y), (ir, ig, ib))
+                bar()
     image.save(filename)
