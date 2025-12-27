7fa957f398b1a1620ed85ef5794a4170dd81c8d2
Roy Moore <roy@moore.co.il>
2025-12-28 00:19:39 +0200

    Refactor to main

--- gradient.py
+++ gradient.py
@@ -10,13 +10,11 @@ def ray_color(r: ray) -> color:
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)
 
 # Generate a gradient image and save it to a file
-def generate_gradient_image(width=256, height=256, filename="gradient.png"):
-    image = Image.new("RGB", (width, height))
-
+def generate_gradient_image(image: Image, filename: str):
     # Camera setup
     focal_length = 1.0
     viewport_height = 2.0
-    viewport_width = viewport_height * (width / height)
+    viewport_width = viewport_height * (image.width / image.height)
     camera_center = point3(0, 0, 0)
 
     # calculate horizontal and vertical vectors
@@ -24,15 +22,15 @@ def generate_gradient_image(width=256, height=256, filename="gradient.png"):
     viewport_v : vec3 = vec3(0, -viewport_height, 0)
 
     # calculate horizontal and vertical vectors from pixel to pixel
-    pixel_delta_u: vec3 = viewport_u / width
-    pixel_delta_v: vec3 = viewport_v / height
+    pixel_delta_u: vec3 = viewport_u / image.width
+    pixel_delta_v: vec3 = viewport_v / image.height
 
     # calculate upper left corner of the viewport
     viewport_upper_left = (camera_center - vec3(0,0,focal_length) - viewport_u / 2 - viewport_v / 2)
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)
-    with alive_bar(width * height, title="Generating Gradient Image") as bar:
-        for x in range(width):
-            for y in range(height):
+    with alive_bar(image.width * image.height, title="Generating Gradient Image") as bar:
+        for x in range(image.width):
+            for y in range(image.height):
                 pixel_center = pixel00_loc + (x * pixel_delta_u) + (y * pixel_delta_v)
                 ray_direction = pixel_center - camera_center
                 r: ray = ray(camera_center, ray_direction)
@@ -41,5 +39,6 @@ def generate_gradient_image(width=256, height=256, filename="gradient.png"):
                 write_color(image, x, y, pixel_color)
                 
                 bar()
+
     image.save(filename)
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from gradient import generate_gradient_image
 
 def main():
@@ -5,7 +6,9 @@ def main():
     image_width = 400
     image_height = int(image_width / aspect_ratio)
     image_height = 1 if image_height < 1 else image_height
-    generate_gradient_image(width=image_width, height=image_height, filename="gradient.png")
+
+    image = Image.new("RGB", (image_width, image_height))
+    generate_gradient_image(image, "gradient.png")
 
 if __name__ == "__main__":
     main()
