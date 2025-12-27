5d984fa4139aa59e93bcc44901cf340ea31f1f65
Roy Moore <roy@moore.co.il>
2025-12-27 23:20:29 +0200

    Fix typo

--- color.py
+++ color.py
@@ -2,10 +2,10 @@ from vec3 import vec3
 
 color = vec3  # Alias for clarity
 
-def write_color(pixeel_color: color):
-    r = pixeel_color.x
-    g = pixeel_color.y
-    b = pixeel_color.z
+def write_color(pixel_color: color):
+    r = pixel_color.x
+    g = pixel_color.y
+    b = pixel_color.z
 
     rbyte = int(255.999 * r)
     gbyte = int(255.999 * g)
